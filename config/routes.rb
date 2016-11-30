6a5e546584c7cb793a90ee56bc5b310913895d57
Leah <leahstuckey@me.com>
2016-11-29 19:11:58 -0600

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-    resources :users do
-      resources :lists
-    end
-
-    resources :lists, only: [] do
-      resources :items, only: [:create, :update]
-    end
+    
   end
