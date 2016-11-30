ac70f84be85a6b7229cdff68a494705ef0e11f86
Leah <leahstuckey@me.com>
2016-11-29 19:43:39 -0600

    FInal commits

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,8 +2,8 @@ class Api::UsersController < ApiController
 
   before_action :authenticated?
 
-     def index
-       users = User.all
-       render json: users, each_serializer: UserSerializer
-     end
+   def index
+     users = User.all
+     render json: users, each_serializer: UserSerializer
+   end
 end
